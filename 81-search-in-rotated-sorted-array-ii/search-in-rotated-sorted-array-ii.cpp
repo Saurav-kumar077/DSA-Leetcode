4f35e1445e1ca8874683c2429fc66e6f95a86eb5
saurav kumar <126255878+Saurav-kumar077@users.noreply.github.com>
2025-08-21 12:20:06 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (20.17%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,37 +1,32 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        int st = 0;
-        int end = nums.size() - 1;
-        while (st <= end) {
-            int mid = st + (end - st) / 2;
-
-            if (nums[mid] == target) {
+        int n =nums.size();
+        int st=0;
+        int end=n-1;
+        while(st<=end){
+            int mid =st+(end-st)/2;
+            if(nums[mid]==target){
                 return true;
             }
-
-            // --- The Fix ---
-            // Handle the ambiguous case where you can't tell which part is sorted.
-            if (nums[st] == nums[mid]) {
-                st++; // Shrink search space from the left
-                continue; // Continue to the next iteration
+            if(nums[st]==nums[mid]){
+                st++;
+                continue;
             }
-
-            // Now, one of the halves must be sorted.
-            // Check if the left part is sorted
-            if (nums[st] <= nums[mid]) {
-                if (target >= nums[st] && target < nums[mid]) {
-                    end = mid - 1;
-                } else {
-                    st = mid + 1;
+            if(nums[st]<=nums[mid]){
+                if(target>=nums[st] && nums[mid]>target){
+                    end=mid-1;
                 }
-            } 
-            // Otherwise, the right part is sorted
-            else {
-                if (target > nums[mid] && target <= nums[end]) {
-                    st = mid + 1;
-                } else {
-                    end = mid - 1;
+                else{
+                    st=mid+1;
+                }
+            }
+            else{
+                if(nums[mid]<target && nums[end]>=target){
+                    st=mid+1;
+                }
+                else{
+                    end=mid-1;
                 }
             }
         }
