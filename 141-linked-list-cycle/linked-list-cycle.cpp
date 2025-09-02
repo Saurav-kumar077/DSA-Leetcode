f593a5269863c0b300231490cab56cb3ec0dd6a3
saurav kumar <126255878+Saurav-kumar077@users.noreply.github.com>
2025-09-02 15:30:22 +0530

    Time: 8 ms (80.45%) | Memory: 11.8 MB (53.28%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -13,8 +13,9 @@ public:
         ListNode* fast = head;
 
         while(fast !=NULL && fast->next !=NULL){
-            slow = slow->next ; 
             fast = fast->next->next;
+            slow = slow->next ; 
+            
 
             if(fast == slow){
                 return true;
