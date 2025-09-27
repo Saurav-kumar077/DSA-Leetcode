a34165a81679b38c5aac0dd5a6a5859792caebad
saurav kumar <126255878+Saurav-kumar077@users.noreply.github.com>
2025-09-27 12:50:02 +0530

    Time: 13 ms (44.11%) | Memory: 17.4 MB (67.24%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -10,27 +10,21 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root==nullptr){
-            return nullptr;
+        if(root==NULL){
+            return NULL;
         }
-        if((root ==p)||(root ==q)){
+        if(root->val==p->val || root->val==q->val){
             return root;
         }
-        TreeNode* leftAns=lowestCommonAncestor(root->left,p,q);
-        TreeNode* rightAns=lowestCommonAncestor(root->right,p,q);
+        TreeNode* leftLCA = lowestCommonAncestor(root->left , p , q);
+        TreeNode* rightLCA = lowestCommonAncestor(root->right , p , q);
 
-        if(leftAns!=nullptr && rightAns!=nullptr){
+        if(leftLCA && rightLCA){
             return root;
-        }
-        else if(leftAns!= nullptr && rightAns==nullptr){
-            return leftAns;
-        }
-        else if(leftAns == nullptr && rightAns!= nullptr){
-            return rightAns;
-        }
-        else
-        {
-            return nullptr;
+        }else if(leftLCA !=NULL){
+            return leftLCA;
+        }else{
+            return rightLCA;
         }
 
     }
