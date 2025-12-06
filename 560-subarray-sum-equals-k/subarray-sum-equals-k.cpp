9d6d990cfc35d395b5277dd2dcb11423ede89ce0
saurav kumar <126255878+Saurav-kumar077@users.noreply.github.com>
2025-12-06 12:40:50 +0530

    Time: 34 ms (90.51%) | Memory: 45.3 MB (69.52%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int n=nums.size();
-        int count =0;
-        
-        for(int i=0;i<n;i++){
-            int current_sum=0;
-            for(int j=i;j<n;j++){
-                current_sum +=nums[j];
-                if(current_sum==k){
-                    count++;
-                }
+        unordered_map<int,int>map;
+        map[0]=1; //0 already exists once
+        int prefixsum=0;
+        int countSubarrays = 0;
+
+        for(int x:nums){
+            prefixsum += x;
+
+            int need = prefixsum-k;
+            if(map.find(need)!=map.end()){
+                countSubarrays +=map[need];
             }
+            map[prefixsum]++;
         }
-        return count;
+        return countSubarrays;
+
     }
 };
