6eae1f8e1de9d5c5fadddd04734af8311ee3767d
saurav kumar <126255878+Saurav-kumar077@users.noreply.github.com>
2025-12-17 12:11:39 +0530

    Time: 27 ms (38.05%) | Memory: 102.9 MB (72.88%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -3,7 +3,6 @@ public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
         int n= temperatures.size();
         vector<int>ans(n);
-        ans[n-1]=0;
         stack<int>st;
         st.push(n-1);
         for(int i=n-2;i>=0;i--){
